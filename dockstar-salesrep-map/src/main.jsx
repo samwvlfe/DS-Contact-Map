1ca36cf4285f7879140757f1e85e845285709ef9
samwvlfe <r.sam.wolfe@gmail.com>
2026-05-04 18:36:45 -0400

    fix import

--- dockstar-salesrep-map/src/main.jsx
+++ dockstar-salesrep-map/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import AuthSuccess from './pages/AuthSuccess.jsx'
+import AuthSuccess from './pages/authSuccess.jsx'
 import { UserProvider } from './context/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
